4cfafb9ac850ab1e9f595e8e4f8d4b6fe8bf5c3e
Will Koste <willkoste@gmail.com>
2020-04-10 11:39:35 -0400

    Delete User & Profile

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -128,4 +128,21 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+// @route:  DELETE, /api/profile
+// @desc:   Delete profile by user id
+// @access: Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    await Profile.findOneAndRemove({user: req.user.id});
+
+    await User.findOneAndRemove({_id: req.user.id});
+    
+    res.json({msg: 'User and profile were deleted'});
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
