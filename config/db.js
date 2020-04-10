9164af8db322ae35bef458534d08ca458e62529b
Will Koste <willkoste@gmail.com>
2020-04-10 11:26:30 -0400

    Get profile by user id

--- config/db.js
+++ config/db.js
@@ -7,7 +7,8 @@ const connectDB = async () => {
     mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true
+      useCreateIndex: true,
+      useFindAndModify: false
     });
 
     console.log('Mongo is connected right now good job^^^^');
