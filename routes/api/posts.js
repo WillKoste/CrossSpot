5e914e8d6034671babccb9f727ae2fa6cb7531bc
Will Koste <willkoste@gmail.com>
2020-04-13 11:58:48 -0400

    Delete Comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -177,4 +177,34 @@ router.post('/comment/:id', [auth, [
   }
 });
 
+// @route:  DELETE, /api/posts/comment/:id/:comment_id
+// @desc:   Comment on a post
+// @access: Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+
+    if(!comment){
+      return res.status(404).json({msg: 'Comment not found'});
+    }
+
+    if(comment.user.toString() !== req.user.id){
+      return res.status(401).json({msg: 'Authorization Denied'});
+    }
+
+    const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id);
+
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
