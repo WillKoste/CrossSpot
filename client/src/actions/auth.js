5c6a109d2de86ec652129999f468c22c353d0009
Will Koste <willkoste@gmail.com>
2020-04-15 11:51:40 -0400

    User Loaded & Auth Error

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,26 @@
-import {REGISTER_SUCCESS, REGISTER_FAIL} from './types';
+import {REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR} from './types';
 import axios from 'axios';
+import setAuthToken from '../utils/setAuthToken';
 import {setAlert} from './alert';
+
+export const loadUser = () => async dispatch => {
+  if(localStorage.token){
+    setAuthToken(localStorage.token);
+  }
+
+  try {
+    const res = await axios.get('/api/auth');
+
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: AUTH_ERROR
+    });
+  }
+}
 
 export const register = ({name, email, password}) => async dispatch => {
   const config = {
