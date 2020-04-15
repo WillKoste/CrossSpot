4c33c2e7cbaf8efb0db54dd3979a2e3f2736c44c
Will Koste <willkoste@gmail.com>
2020-04-15 12:08:32 -0400

    Login And Redirect

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,4 +1,4 @@
-import {REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR} from './types';
+import {REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL } from './types';
 import axios from 'axios';
 import setAuthToken from '../utils/setAuthToken';
 import {setAlert} from './alert';
@@ -38,6 +38,8 @@ export const register = ({name, email, password}) => async dispatch => {
       type: REGISTER_SUCCESS,
       payload: res.data
     });
+
+    dispatch(loadUser());
   } catch (err) {
     const errors = err.response.data.errors
 
@@ -47,6 +49,37 @@ export const register = ({name, email, password}) => async dispatch => {
     
     dispatch({
       type: REGISTER_FAIL
+    });
+  }
+}
+
+export const login = (email, password) => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  const body = JSON.stringify({email, password});
+
+  try {
+    const res = await axios.post('/api/auth', body, config);
+    
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data
+    });
+
+    dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors
+
+    if(errors){
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+    
+    dispatch({
+      type: LOGIN_FAIL
     });
   }
 }
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, {Fragment, useState} from 'react';
-import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {Link, Redirect} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import {login} from '../../actions/auth';
 
-const Login = () => {
+const Login = ({login, isAuthenticated}) => {
   const [formData, setFormData] = useState({
     email: '',
     password: ''
@@ -14,7 +17,11 @@ const Login = () => {
   const onSubmit = async e => {
     e.preventDefault();
     
-    console.log('Logged in successfully');
+    login(email, password);
+  }
+
+  if(isAuthenticated){
+    return <Redirect to="/dashboard" />
   }
   
   return (
@@ -35,4 +42,13 @@ const Login = () => {
   )
 }
 
-export default Login;
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+}
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, {login})(Login);
