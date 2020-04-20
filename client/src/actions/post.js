9a20719bcab478e49c66a8f23dfed1372e0b6767
Will Koste <willkoste@gmail.com>
2020-04-20 11:42:59 -0400

    Add Post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,4 +1,4 @@
-import {GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST} from './types';
+import {GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST, ADD_POST} from './types';
 import axios from 'axios';
 import {setAlert} from './alert';
 
@@ -60,6 +60,28 @@ export const deletePost = (id) => async dispatch => {
     });
 
     dispatch(setAlert('Post Removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {msg: err.response.statusText, status: err.response.status}
+    });
+  }
+}
+
+export const addPost = (formData) => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+  
+  try {
+    const res = await axios.post('/api/posts', formData, config);
+
+    dispatch({
+      type: ADD_POST,
+      payload: res.data
+    });
   } catch (err) {
     dispatch({
       type: POST_ERROR,
--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import {getPosts} from '../../actions/post';
 import Spinner from '../layout/Spinner';
 import PostItem from './PostItem';
+import PostForm from './PostForm';
 
 const Posts = ({getPosts, post: {posts, loading}}) => {
   useEffect(() => {
@@ -13,7 +14,7 @@ const Posts = ({getPosts, post: {posts, loading}}) => {
   return loading ? <Spinner /> : (<Fragment>
     <h1 className="large text-primary">Posts</h1>
     <p className="lead"><i className="fas fa-user" /> Welcome to the community!</p>
-    {/* PostForm */}
+    <PostForm />
     <div className="posts">
       {posts.map(post => (
         <PostItem key={post._id} post={post} />
