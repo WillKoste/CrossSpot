9521f6c93711cd628ce0ce45468b5c0be9e40a03
Will Koste <willkoste@gmail.com>
2020-04-21 10:01:15 -0400

    Production

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const auth = require('./routes/api/auth');
 const profiles = require('./routes/api/profiles');
 const posts = require('./routes/api/posts');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -11,14 +12,18 @@ connectDB();
 
 app.use(express.json({extended: false}));
 
-app.get('/', (req, res) => {
-  res.send('You are a boss man, you can do it');
-});
-
 app.use('/api/users', users);
 app.use('/api/auth', auth);
 app.use('/api/profile', profiles);
 app.use('/api/posts', posts);
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 const PORT = process.env.port || 5000;
 
